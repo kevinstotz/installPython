bf1a2ec5b2740118df883ebc4db42037366455d4
Kevin Stotz <kevin_stotz@yahoo.com>
2017-07-29 22:16:42 -0400

    Update installPython.sh

--- installPython.sh
+++ installPython.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-sudo yum -y groupinstall "Development tools"
-sudo yum -y install zlib-devel openssl-devel
+sudo apt -y install gcc
+sudo apt -y install groupinstall "Development tools"
+sudo apt -y install zlib-devel openssl-devel
 wget https://www.python.org/ftp/python/$1/Python-$1.tar.xz
 tar xJf Python-$1.tar.xz
 cd Python-$1
 ./configure --prefix=/usr/local
-sudo make install
+sudo make -y install
 python3 --version
