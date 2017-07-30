f8e67cdb1537d292c56e94cc99e1fa43de977fed
Kevin Stotz <kevin_stotz@yahoo.com>
2017-07-29 22:28:35 -0400

    Update installPython.sh

--- installPython.sh
+++ installPython.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+md gcc
+cd gcc
 sudo apt -y install gcc
+cd ..
+md make
+cd make
+sudo apt -y install make
+cd ..
 sudo apt -y install groupinstall "Development tools"
 sudo apt -y install zlib-devel openssl-devel
 wget https://www.python.org/ftp/python/$1/Python-$1.tar.xz
